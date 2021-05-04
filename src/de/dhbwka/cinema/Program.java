7d32b928f7a29d49baae5c69576a49432b5e6e01
Gimleux <56401687+Gimleux@users.noreply.github.com>
2021-05-04 15:48:32 +0200

    change Program Input

--- src/de/dhbwka/cinema/Program.java
+++ src/de/dhbwka/cinema/Program.java
@@ -1,7 +1,5 @@
 package de.dhbwka.cinema;
 
-import java.lang.*;
-
 /**
  * Note that Java console applications need to be run through the java runtime
  * by running "java -jar JarFile.jar" in the command line.
@@ -18,11 +16,11 @@ public class Program
     {
         String result;
         System.out.println("Welcome to the de.dhbwka.cinema.Movie Store");
-        Movie m1 = new Movie("movie1", 1);
-        Movie m2 = new Movie("movie2", 2);
+        Movie m1 = new Movie("The Hunger Games", Movie.REGULAR);
+        Movie m2 = new Movie("The Hunger Games - Catching Fire", Movie.NEW_RELEASE);
         Rental r1 = new Rental(m1, 10);
         Rental r2 = new Rental(m2, 5);
-        Customer c1 = new Customer("joe");
+        Customer c1 = new Customer("Katniss Everdeen");
         c1.addRental(r1);   c1.addRental(r2);   
         System.out.println("Let's get the Statement");
         result = c1.statement();
